778911a10c6c6d2daeda01a6142f4be0ac7e6f26
JonathanGrant <jonathanallengrant@gmail.com>
2016-09-10 01:38:03 -0700

    Added lyft

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = new express();
 var yelp = require('./app/middleware/yelp');
+var lyft = require('./app/middleware/lyft');
 
-app.get('/api/random', [yelp], function(req, res) {
+app.get('/api/random', [yelp, lyft], function(req, res) {
   if (req.err) res.status(req.err)
   else {
     res.status(200);
     res.send({
-      'yelp': req.yelp
+      'yelp': req.yelp,
+      'lyft': req.lyft
     });
   }
 });
